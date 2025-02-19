febe535fca5e1778443bf86e65538cd34780d039
Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>
2025-02-19 14:14:02 +0000

    fix: add typeDefs

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,4 +1,41 @@
 import { gql } from 'apollo-server';
 
 export const typeDefs = gql`
+  type Ingredient {
+    id: ID!
+    name: String!
+    supplier: String!
+    latestPrice: Float!
+  }
+
+  type PriceChange {
+    changedAt: String!
+    price: Float!
+  }
+
+  type Recipe {
+    id: ID!
+    name: String!
+    ingredients: [RecipeIngredient!]!
+    totalCost: Float!
+  }
+
+  type RecipeIngredient {
+    ingredient: Ingredient!
+    quantity: Float!
+  }
+
+  type Query {
+    ingredients: [Ingredient!]!
+    recipes: [Recipe!]!
+  }
+
+  type Mutation {
+    createRecipe(name: String!, ingredients: [RecipeIngredientInput!]!): Recipe!
+  }
+
+  input RecipeIngredientInput {
+    ingredientId: ID!
+    quantity: Float!
+  }
 `;
