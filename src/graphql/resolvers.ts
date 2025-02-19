e05ecbf24c9887c4c620a2e6afb07ea24dc0e6ed
Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>
2025-02-19 14:13:23 +0000

    fix: fix resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -2,6 +2,47 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const resolvers = {
+interface CreateRecipeArgs {
+  name: string;
+  ingredients: { ingredientId: number; quantity: number }[];
+}
 
+const resolvers = {
+  Query: {
+    recipes: async () => {
+      return await prisma.recipe.findMany({
+        include: {
+          ingredients: {
+            include: {
+              ingredient: true,
+            },
+          },
+        },
+      });
+    },
+  },
+  Mutation: {
+    createRecipe: async (_: unknown, { name, ingredients }: CreateRecipeArgs) => {
+      return await prisma.recipe.create({
+        data: {
+          name,
+          ingredients: {
+            create: ingredients.map(({ ingredientId, quantity }) => ({
+              ingredient: { connect: { id: ingredientId } },
+              quantity,
+            })),
+          },
+        },
+        include: {
+          ingredients: {
+            include: {
+              ingredient: true,
+            },
+          },
+        },
+      });
+    },
+  },
 };
+
+export default resolvers;
